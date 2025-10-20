14b834d3c7a3171b022f8185d2635a6262297c53
alissonsveen <alissongamer6@gmail.com>
2025-10-20 15:33:16 -0300

    feat: Testing returning question details

--- src/domain/forum/application/use-cases/comment-on-answer.spec.ts
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -1,30 +1,35 @@
-import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
-import { makeAnswer } from 'test/factories/make-answer'
-import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
-import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
-import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository"
+import { makeAnswer } from "test/factories/make-answer"
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository"
+import { CommentOnAnswerUseCase } from "@/domain/forum/application/use-cases/comment-on-answer"
+import { InMemoryAnswerAttachmentsRepository } from "test/repositories/in-memory-answer-attachments-repository"
+import { InMemoryStudentsRepository } from "test/repositories/in-memory-students-repository"
 
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let inMemoryStudentsRepository: InMemoryStudentsRepository
 let sut: CommentOnAnswerUseCase
 
-describe('Comment on Answer', () => {
+describe("Comment on Answer", () => {
   beforeEach(() => {
     inMemoryAnswerAttachmentsRepository =
       new InMemoryAnswerAttachmentsRepository()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
     inMemoryAnswersRepository = new InMemoryAnswersRepository(
-      inMemoryAnswerAttachmentsRepository,
+      inMemoryAnswerAttachmentsRepository
     )
-    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository(
+      inMemoryStudentsRepository
+    )
 
     sut = new CommentOnAnswerUseCase(
       inMemoryAnswersRepository,
-      inMemoryAnswerCommentsRepository,
+      inMemoryAnswerCommentsRepository
     )
   })
 
-  it('should be able to comment on answer', async () => {
+  it("should be able to comment on answer", async () => {
     const answer = makeAnswer()
 
     await inMemoryAnswersRepository.create(answer)
@@ -32,11 +37,11 @@ describe('Comment on Answer', () => {
     await sut.execute({
       answerId: answer.id.toString(),
       authorId: answer.authorId.toString(),
-      content: 'Comentário teste',
+      content: "Comentário teste",
     })
 
     expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
-      'Comentário teste',
+      "Comentário teste"
     )
   })
 })
--- src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
@@ -1,31 +1,39 @@
-import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
-import { makeQuestion } from 'test/factories/make-question'
-import { FetchRecentQuestionsUseCase } from './fetch-recent-questions'
-import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository"
+import { makeQuestion } from "test/factories/make-question"
+import { FetchRecentQuestionsUseCase } from "./fetch-recent-questions"
+import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository"
+import { InMemoryStudentsRepository } from "test/repositories/in-memory-students-repository"
+import { InMemoryAttachmentsRepository } from "test/repositories/in-memory-attachments-repository"
 
 let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
+let inMemoryStudentsRepository: InMemoryStudentsRepository
 let sut: FetchRecentQuestionsUseCase
 
-describe('Fetch Recent Questions', () => {
+describe("Fetch Recent Questions", () => {
   beforeEach(() => {
     inMemoryQuestionAttachmentsRepository =
       new InMemoryQuestionAttachmentsRepository()
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
       inMemoryQuestionAttachmentsRepository,
+      inMemoryAttachmentsRepository,
+      inMemoryStudentsRepository
     )
     sut = new FetchRecentQuestionsUseCase(inMemoryQuestionsRepository)
   })
 
-  it('should be able to fetch recent questions', async () => {
+  it("should be able to fetch recent questions", async () => {
     await inMemoryQuestionsRepository.create(
-      makeQuestion({ createdAt: new Date(2022, 0, 20) }),
+      makeQuestion({ createdAt: new Date(2022, 0, 20) })
     )
     await inMemoryQuestionsRepository.create(
-      makeQuestion({ createdAt: new Date(2022, 0, 18) }),
+      makeQuestion({ createdAt: new Date(2022, 0, 18) })
     )
     await inMemoryQuestionsRepository.create(
-      makeQuestion({ createdAt: new Date(2022, 0, 23) }),
+      makeQuestion({ createdAt: new Date(2022, 0, 23) })
     )
 
     const result = await sut.execute({
@@ -39,7 +47,7 @@ describe('Fetch Recent Questions', () => {
     ])
   })
 
-  it('should be able to fetch paginated recent questions', async () => {
+  it("should be able to fetch paginated recent questions", async () => {
     for (let i = 1; i <= 22; i++) {
       await inMemoryQuestionsRepository.create(makeQuestion())
     }
