fd141e32d478cca37a94f79296c88013574dfbf3
alissonsveen <alissongamer6@gmail.com>
2025-10-20 10:10:31 -0300

    feat: answers with attachments test, repository & controllers

--- src/infra/http/controllers/create-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-question.controller.e2e-spec.ts
@@ -57,6 +57,7 @@ describe("Create question (E2E)", () => {
     })
 
     expect(questionOnDatabase).toBeTruthy()
+
     const attachmentsOnDatabase = await prisma.attachment.findMany({
       where: {
         questionId: questionOnDatabase?.id,
